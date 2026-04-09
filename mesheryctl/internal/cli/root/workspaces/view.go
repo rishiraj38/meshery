b69a594b7cd970a73faa43e1c31d9df5b04fb7f3
Pragalva Sapkota <sapkotapragalva@gmail.com>
2026-04-09 22:16:57 +0545

    [mesheryctl] Fix: workspace view command failing even with valid orgId
    Signed-off-by: Pragalva Sapkota <sapkotapragalva@gmail.com>

--- mesheryctl/internal/cli/root/workspaces/view.go
+++ mesheryctl/internal/cli/root/workspaces/view.go
@@ -17,7 +17,7 @@ import (
 type workspaceViewFlags struct {
 	OutputFormat string `json:"output-format" validate:"required,oneof=json yaml"`
 	Save         bool   `json:"save" validate:"boolean"`
-	OrgID        string `json:"orgId" validate:"omitempty,uuid"`
+	OrgID        string `json:"orgId" validate:"required,uuid"`
 }
 
 var workspaceViewFlagsProvided workspaceViewFlags
@@ -37,16 +37,16 @@ var viewWorkspaceCmd = &cobra.Command{
 Find more information at: https://docs.meshery.io/reference/mesheryctl/workspace/view`,
 	Example: `
 // View details of a specific workspace by ID
-mesheryctl workspace view [workspace-id]
+mesheryctl workspace view [workspace-id] --orgId [orgId]
 
-// View details of a specific workspace by name (requires --orgId)
+// View details of a specific workspace by name
 mesheryctl workspace view [workspace-name] --orgId [orgId]
 
 // View details of a specific workspace in JSON format
-mesheryctl workspace view [workspace-id] --output-format json
+mesheryctl workspace view [workspace-id] --orgId [orgId] --output-format json
 
 // View details of a specific workspace and save it to a file
-mesheryctl workspace view [workspace-id] --output-format json --save
+mesheryctl workspace view [workspace-id] --orgId [orgId] --output-format json --save
 	`,
 	PreRunE: func(cmd *cobra.Command, args []string) error {
 		return mesheryctlflags.ValidateCmdFlags(cmd, &workspaceViewFlagsProvided)
@@ -57,9 +57,8 @@ mesheryctl workspace view [workspace-id] --output-format json --save
 			return utils.ErrInvalidArgument(fmt.Errorf("please provide exactly one workspace name or ID\n\n%v", errMsg))
 		}
 
-		// Validate orgId is provided when arg is not a UUID
-		if !utils.IsUUID(args[0]) && workspaceViewFlagsProvided.OrgID == "" {
-			return utils.ErrInvalidArgument(fmt.Errorf("--orgId is required when searching by name\n\nUsage: mesheryctl workspace view [workspace-name] --orgId [orgId]"))
+		if workspaceViewFlagsProvided.OrgID == "" {
+			return utils.ErrInvalidArgument(fmt.Errorf("--orgId is required\n\nUsage: mesheryctl workspace view [workspace-name|workspace-id] --orgId [orgId]"))
 		}
 		return nil
 	},
@@ -71,7 +70,7 @@ mesheryctl workspace view [workspace-id] --output-format json --save
 		var displayData display.DisplayDataAsync
 
 		if utils.IsUUID(workspaceNameOrID) {
-			urlPath = fmt.Sprintf("%s/%s", workspacesApiPath, url.PathEscape(workspaceNameOrID))
+			urlPath = fmt.Sprintf("%s/%s?orgID=%s", workspacesApiPath, url.PathEscape(workspaceNameOrID), url.QueryEscape(workspaceViewFlagsProvided.OrgID))
 			fetchedWorkspace, err := api.Fetch[workspace.Workspace](urlPath)
 			if err != nil {
 				return err
@@ -101,7 +100,7 @@ mesheryctl workspace view [workspace-id] --output-format json --save
 				return err
 			}
 
-			urlPath = fmt.Sprintf("%s/%s", workspacesApiPath, url.PathEscape(selectedAvailableWorkspace.ID.String()))
+			urlPath = fmt.Sprintf("%s/%s?orgID=%s", workspacesApiPath, url.PathEscape(selectedAvailableWorkspace.ID.String()), url.QueryEscape(workspaceViewFlagsProvided.OrgID))
 			fetchedWorkspace, err := api.Fetch[workspace.Workspace](urlPath)
 			if err != nil {
 				return err
@@ -147,5 +146,5 @@ mesheryctl workspace view [workspace-id] --output-format json --save
 func init() {
 	viewWorkspaceCmd.Flags().StringVarP(&workspaceViewFlagsProvided.OutputFormat, "output-format", "o", "yaml", "(optional) format to display in [json|yaml]")
 	viewWorkspaceCmd.Flags().BoolVarP(&workspaceViewFlagsProvided.Save, "save", "s", false, "(optional) save output as a JSON/YAML file")
-	viewWorkspaceCmd.Flags().StringVarP(&workspaceViewFlagsProvided.OrgID, "orgId", "", "", "(optional) organization ID to search workspace by name")
+	viewWorkspaceCmd.Flags().StringVarP(&workspaceViewFlagsProvided.OrgID, "orgId", "", "", "(required) organization ID")
 }
